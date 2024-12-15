541325469d95088e398d29345cdeae3a0c722d03
khawla lamsiyeh <61538329+khawlalam@users.noreply.github.com>
2024-12-15 10:16:21 +0100

    add comment

--- src/main/java/ws/ServerJWS.java
+++ src/main/java/ws/ServerJWS.java
@@ -2,10 +2,20 @@ package ws;
 
 import jakarta.xml.ws.Endpoint;
 
+/**
+ * This class serves as a standalone server for deploying a Java-based web service.
+ * It uses the `Endpoint` class from Jakarta XML Web Services (JAX-WS) to publish the
+ * `BanqueService` web service at a specified URL.
+ */
 public class ServerJWS {
     public static void main(String[] args) {
-        String url="http://0.0.0.0:8787/";
-        Endpoint.publish(url,new BanqueService());
-        System.out.println("Web service deployé sur  "+url);
+        // Define the URL where the web service will be deployed.
+        String url = "http://0.0.0.0:8787/";
+
+        // Publish the `BanqueService` web service at the specified URL.
+        Endpoint.publish(url, new BanqueService());
+
+        // Log a confirmation message indicating successful deployment.
+        System.out.println("Web service deployé sur  " + url);
     }
 }
